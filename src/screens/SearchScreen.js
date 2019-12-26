8029ad501d14a4f627a182bb9c00ff1c8f1b59d8
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-26 10:37:20 -0800

    add in helper function to filter prices.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -7,6 +7,14 @@ import ResultsList from "../components/ResultsList";
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
   const [searchAPI, results, errorMessage] = useResults();
+
+  const filterResultsByPrice = price => {
+    //price is going to be equal to $ or $$ or $$$
+    return results.filter(result => {
+      return result.price === price;
+    });
+  };
+
   return (
     <View>
       <SearchBar
@@ -16,9 +24,16 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length}</Text>
-      <ResultsList title="CostEffective" />
-      <ResultsList title="Bit Pricier" />
-      <ResultsList title="Hey Big Spender" />
+      <ResultsList results={filterResultsByPrice("$")} title="CostEffective" />
+      <ResultsList results={filterResultsByPrice("$$")} title="Bit Pricier" />
+      <ResultsList
+        results={filterResultsByPrice("$$$")}
+        title="Hey Big Spender"
+      />
+      <ResultsList
+        results={filterResultsByPrice("$$$$")}
+        title="Too Rich to Care"
+      />
     </View>
   );
 };
