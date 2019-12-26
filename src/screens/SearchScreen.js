3becd70d22de3b489070817557a9cb4ca98b8726
Mike Lofgren <mikeylofgren@gmail.com>
2019-12-26 09:53:33 -0800

    hook up results list component to SearchScreen.js.

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
+import ResultsList from "../components/ResultsList";
 
 const SearchScreen = () => {
   const [term, setTerm] = useState("");
@@ -15,6 +16,9 @@ const SearchScreen = () => {
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length}</Text>
+      <ResultsList />
+      <ResultsList />
+      <ResultsList />
     </View>
   );
 };
